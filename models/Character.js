5acc2cca037b3f431c0127260c92ff07b83cd4a0
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-10-27 11:39:16 -0400

    updated the models character.js file

--- models/Character.js
+++ models/Character.js
@@ -5,15 +5,11 @@ const Schema = mongoose.Schema;
 let CharacterSchema = new Schema({
   _id: { type: String },
   name: { type: String },
-  role: { type: String },
-  house: { type: String },
-  school: { type: String },
-  ministryOfMagic: { type: Boolean },
-  orderOfThePhoenix: { type: Boolean },
-  dumbledoresArmy: { type: Boolean },
-  deathEater: { type: Boolean },
-  bloodStatus: { type: String },
-  species: { type: String },
+  japaneseName: { type: String },
+  englishName: { type: String },
+  moon: { type: String },
+  image: { type: Boolean },
+  
 });
 
 export default mongoose.model("characters", CharacterSchema);
